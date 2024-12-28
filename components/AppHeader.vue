ee96cf400a567e686cfa4cfcfd13f7716f10f093
unknown <sonesdv164@gmail.com>
2024-12-28 13:00:33 +0700

    avantar

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,5 +7,74 @@
     </template>
 
     <v-app-bar-title>Application Bar</v-app-bar-title>
+    <v-menu
+        min-width="200px"
+        rounded
+      >
+        <template v-slot:activator="{ props }">
+          <v-btn
+            icon
+            v-bind="props"
+          >
+            <v-avatar
+            class="pa-3,mr-3 "
+              color="#BDBDBD"
+              size="large"
+            >
+              <span class="text-h5" v-if="user.initials">
+                <!-- {{ user.initials }} -->
+                <v-icon icon="mdi-account-circle-outline" color="#FFFFFF"></v-icon>
+
+              </span>
+              <span class="text-h5" v-else>
+                  <v-icon icon="mdi-account-circle-outline" color="#FFFFFF"></v-icon>
+                </span>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-text>
+            <div class="mx-auto text-center">
+              <v-avatar
+                color="brown"
+              >
+                <span class="text-h5" v-if="user.initials">{{ user.initials }}</span>
+                <span class="text-h5" v-else>
+                  <v-icon icon="mdi-account-circle-outline"></v-icon>
+                </span>
+              </v-avatar>
+              <h3 >{{ user.fullName }}</h3>
+              <p class="text-caption mt-1">
+                {{ user.email }}
+              </p>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                variant="text"
+                rounded
+              >
+                Edit Account
+              </v-btn>
+              <v-divider class="my-3"></v-divider>
+              <v-btn
+                variant="text"
+                rounded
+              >
+                Disconnect
+              </v-btn>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-menu>
   </v-app-bar>
 </template>
+<script>
+  export default {
+    data: () => ({
+      user: {
+        initials: 'S',
+        fullName: 'Sone SEEDAVANH',
+        email: 'sone@gmail.com',
+      },
+    }),
+  }
+</script>
--- components/Login/index.vue
+++ components/Login/index.vue
@@ -1,13 +1,20 @@
 <template>
   <v-container>
     <div class="text-center mb-5">
-      <v-avatar color="surface-variant" size="100"></v-avatar>
+      <v-avatar color="surface-variant" size="150"></v-avatar>
     </div>
     <h5 class="text-center mb-5">ເຂົ້າສູ່ລະບົບ 32 Lottery</h5>
     <div class="d-flex justify-center">
-      <v-form style="width: 50%" class="text-center" @submit.prevent="handleLogin">
-        <label for="username" class="d-flex text-start">ຊື່ຜູ້ໃຊ້ງານ / Username</label>
+      <v-form
+        style="width: 50%"
+        class="text-center"
+        @submit.prevent="handleLogin"
+      >
+        <label for="username" class="d-flex text-start"
+          >ຊື່ຜູ້ໃຊ້ງານ / Username</label
+        >
         <v-text-field
+          class="rounded-lg"
           id="username"
           v-model="username"
           label="ກະລຸນາປ້ອນຊື່ຜູ້ໃຊ້ງານ"
@@ -15,7 +22,9 @@
           variant="outlined"
         ></v-text-field>
 
-        <label for="password" class="d-flex text-start">ລະຫັດຜ່ານ / Password</label>
+        <label for="password" class="d-flex text-start"
+          >ລະຫັດຜ່ານ / Password</label
+        >
         <v-text-field
           id="password"
           v-model="password"
@@ -27,7 +36,12 @@
           variant="outlined"
         ></v-text-field>
 
-        <v-btn class="mt-4" color="primary" variant="elevated" @click="handleLogin">
+        <v-btn
+          class="mt-4 rounded-lg"
+          color="primary"
+          variant="elevated"
+          @click="handleLogin"
+        >
           ເຂົ້າສູ່ລະບົບ
         </v-btn>
       </v-form>
@@ -36,14 +50,14 @@
 </template>
 
 <script lang="ts">
-import Swal from 'sweetalert2';
-import { ref } from 'vue';
+import Swal from "sweetalert2";
+import { ref } from "vue";
 
 export default {
-  name: 'Login',
+  name: "Login",
   setup() {
-    const username = ref('');
-    const password = ref('');
+    const username = ref("");
+    const password = ref("");
     const visible = ref(false);
 
     const toggleVisibility = () => {
@@ -53,21 +67,21 @@ export default {
     const handleLogin = () => {
       if (!username.value || !password.value) {
         Swal.fire({
-          icon: 'error',
-          title: 'ຜິດພາດ',
-          text: 'ກະລຸນາປ້ອນຊື່ຜູ້ໃຊ້ງານ ແລະ ລະຫັດຜ່ານ!',
+          icon: "error",
+          title: "ຜິດພາດ",
+          text: "ກະລຸນາປ້ອນຊື່ຜູ້ໃຊ້ງານ ແລະ ລະຫັດຜ່ານ!",
         });
         return;
       }
 
       Swal.fire({
-        title: 'ກຳລັງເຂົ້າສູ່ລະບົບ',
-        html: 'ກະລຸນາລໍຖ້າ <b></b> ວິທີ',
+        title: "ກຳລັງເຂົ້າສູ່ລະບົບ",
+        html: "ກະລຸນາລໍຖ້າ <b></b> ວິທີ",
         timer: 3000,
         timerProgressBar: true,
         didOpen: () => {
           Swal.showLoading();
-          const b = Swal.getHtmlContainer().querySelector('b');
+          const b = Swal.getHtmlContainer().querySelector("b");
           let timerInterval = setInterval(() => {
             if (b) {
               b.textContent = String(Math.ceil(Swal.getTimerLeft() / 1000));
@@ -78,11 +92,10 @@ export default {
           clearInterval();
         },
       }).then(() => {
-       
         Swal.fire({
-          icon: 'success',
-          title: 'ສໍາເລັດ',
-          text: 'ເຂົ້າລະບົບສໍາເລັດ!',
+          icon: "success",
+          title: "ສໍາເລັດ",
+          text: "ເຂົ້າລະບົບສໍາເລັດ!",
         });
       });
     };
